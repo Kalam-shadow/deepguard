962255f039c13b5fa7987ce806217689f023558f
kalam-shadow <rankholder007@gmail.com>
2025-10-16 20:51:23 +0530

    first commit

--- ShieldX/app/src/main/java/com/example/shieldx/data/NotificationPayload.kt
+++
@@ -1,9 +0,0 @@
-package com.example.shieldx.data
-
-data class NotificationPayload(
-    val source: String,
-    val sender: String,
-    val content: String,
-    val timestamp: Long,
-    val packageName: String? = null
-)
